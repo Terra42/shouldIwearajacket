7ac3076f2cb42416e2a459f8f4b4180d7950e2db
Petra Zemanová <zemape42@gmail.com>
2023-08-20 16:43:06 +0200

    API key added

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import {apiKey} from "./apiKey.js";
 
 const app = express();
 const PORT = 3000;
+const API_URL = "https://api.api-ninjas.com/v1/weather?city=";
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+    
 });
 
 app.listen(PORT, () => {
